0ef02eb1269ea47cfaee7e3be28db22bc22ea145
Raunak Bhansali <rishimehta365@gmail.com>
2020-08-15 19:51:57 +0530

    <passport js> <config> * No need of Google Stratgy anymore in server side

--- src/config/passport.js
+++ src/config/passport.js
@@ -71,41 +71,6 @@ passport.use(
   )
 );
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID:
-        "390242058357-tnqu35rp7bij682da18ubv3edv19tdds.apps.googleusercontent.com",
-      clientSecret: "hV4mCHBG07DoJc5DN7byUFay",
-      callbackURL: "http://localhost:3000/api/v1/vendor/googleOAuth/redirect",
-      passReqToCallback: true,
-    },
-    function (request, accessToken, refreshToken, profile, done) {
-      Vendor.findOne({ email: profile.email })
-        .exec()
-        .then((vendor) => {
-          if (vendor) {
-            done(null, vendor);
-          } else {
-            let vendor = {
-              email: profile.email,
-              email_is_verified: profile.email_verified,
-              firstName: profile.given_name,
-              lastName: profile.family_name,
-            };
-
-            const createVendor = new Vendor(vendor);
-            createVendor
-              .save()
-              .then((data) => done(null, data))
-              .catch((err) => done(err));
-          }
-        })
-        .catch((error) => done(error));
-    }
-  )
-);
-
 passport.serializeUser((customer, done) => {
   done(null, customer.id);
 });
